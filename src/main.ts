61ebef113d9d1153d30a8d78f289809fd1493c8f
sofya <sofyaoliveira100@gmail.com>
2026-01-20 15:10:47 -0300

    fix: Correção render

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+app.enableCors({
+  origin: process.env.CORS_ORIGIN?.split(',') ?? '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
